8c4fab68afe8a3ee58de8f770c7db022720ae8c2
Ethan McCue <emccue@live.com>
2016-05-24 22:08:53 -0400

    Initial. Mult gives a segfault

---
+++ c_template.c
@@ -0,0 +1,18 @@
+#define cell_size 1000
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+//This should be redone with dynamic resizing
+int origin = cell_size/2;
+int x = cell_size/2;
+int y = cell_size/2;
+
+
+int main(){
+    int data[cell_size][cell_size] = {0};
+    srand(time(NULL));
+
+    %s //insertion point for generated code
+return 0;
+}
